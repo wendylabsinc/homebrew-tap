d473cef0ca194a088347a6635200b0945ee0e116
Mihai Chiorean <mihai.v.chiorean@gmail.com>
2025-05-03 14:10:37 -0700

    Update tap to v2025.05.03-175241

--- Formula/edge.rb
+++ Formula/edge.rb
@@ -1,7 +1,7 @@
 class Edge < Formula
   desc "CLI for building and running Edge applications"
   homepage "https://github.com/edgeengineer/edge-agent"
-  url "https://github.com/edgeengineer/edge-agent/archive/refs/tags/v2025.04.25-140637.tar.gz"
+  url "https://github.com/edgeengineer/edge-agent/archive/refs/tags/v2025.05.03-175241.tar.gz"
   sha256 "f3fe63b493a714188367f7d37098be25231e7190e0f99167924a220da7c565a2"
   license "Apache-2.0"
   head "https://github.com/edgeengineer/edge-agent.git", branch: "main"
