1f14b2ed155bdc3629e63601a65063a596ef0513
Mihai Chiorean <mihai.v.chiorean@gmail.com>
2025-05-05 15:47:15 -0700

    New version of the edge cli

--- Formula/edge.rb
+++ Formula/edge.rb
@@ -1,8 +1,8 @@
 class Edge < Formula
   desc "CLI for building and running Edge applications"
   homepage "https://github.com/edgeengineer/edge-agent"
-  url "https://github.com/edgeengineer/edge-agent/archive/refs/tags/v2025.05.03-175241.tar.gz"
-  sha256 "f3fe63b493a714188367f7d37098be25231e7190e0f99167924a220da7c565a2"
+  url "https://github.com/edgeengineer/edge-agent/archive/refs/tags/v2025.05.05-223441.tar.gz"
+  sha256 "524112f4b7cb11314bf12860e27cca294c7537e5"
   license "Apache-2.0"
   head "https://github.com/edgeengineer/edge-agent.git", branch: "main"
 
