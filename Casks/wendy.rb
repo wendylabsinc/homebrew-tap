f4f2129cd26af2326cc6341ec85f4a4fedafc615
Joannis Orlandos <joannis@orlandos.nl>
2025-10-24 21:21:38 +0200

    Separate on_mac and on_linux hierarchically

--- Casks/wendy.rb
+++ Casks/wendy.rb
@@ -6,7 +6,9 @@ cask "wendy" do
   on_macos do
     url "https://github.com/wendylabsinc/wendy-agent/releases/download/2025.10.24-142919/wendy-cli-macos-arm64.tar.gz"
     sha256 "cbe1d957dccc003d84cce2e58040462379fb24cf34016ed07c107c99ad2e7bab"
-  else
+  end
+
+  on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/wendylabsinc/wendy-agent/releases/download/2025.10.24-142919/wendy-cli-linux-static-musl-aarch64.tar.gz"
       sha256 "a52f35021d00acc8d56c42661f14aa27023cfef5f6daeeb3ccb97e1ee1e28f18"
