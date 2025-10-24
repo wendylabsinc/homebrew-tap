e2a6c96378fb7730759a8c7805c1a26a6a6f9b2a
Joannis Orlandos <joannis@orlandos.nl>
2025-10-24 21:19:44 +0200

    Support ask install on Linux

--- Casks/wendy.rb
+++ Casks/wendy.rb
@@ -3,8 +3,18 @@ cask "wendy" do
   desc "CLI for building and running WendyOS applications"
   homepage "https://github.com/wendylabsinc/wendy-agent"
   
-  url "https://github.com/wendylabsinc/wendy-agent/releases/download/2025.10.24-142919/wendy-cli-macos-arm64.tar.gz"
-  sha256 "cbe1d957dccc003d84cce2e58040462379fb24cf34016ed07c107c99ad2e7bab"
+  on_macos do
+    url "https://github.com/wendylabsinc/wendy-agent/releases/download/2025.10.24-142919/wendy-cli-macos-arm64.tar.gz"
+    sha256 "cbe1d957dccc003d84cce2e58040462379fb24cf34016ed07c107c99ad2e7bab"
+  else
+    if Hardware::CPU.arm?
+      url "https://github.com/wendylabsinc/wendy-agent/releases/download/2025.10.24-142919/wendy-cli-linux-static-musl-aarch64.tar.gz"
+      sha256 "a52f35021d00acc8d56c42661f14aa27023cfef5f6daeeb3ccb97e1ee1e28f18"
+    else
+      url "https://github.com/wendylabsinc/wendy-agent/releases/download/2025.10.24-142919/wendy-cli-linux-static-musl-x86_64.tar.gz"
+      sha256 "0b5c740257a1f803d9ad82a4426e626df41ba22ab7c451c3c2b5f455a11048b6"
+    end
+  end
 
   # Install the binary
   binary "wendy-cli-macos-arm64/wendy"
