6b5f02a84ee09adafb0656a8f7091d07e02247dd
Joannis Orlandos <joannis@orlandos.nl>
2025-10-27 12:59:23 +0100

    Add Swiftly dependencies to cask

--- Casks/wendy.rb
+++ Casks/wendy.rb
@@ -4,6 +4,9 @@ cask "wendy" do
   homepage "https://github.com/wendylabsinc/wendy-agent"
   
   on_macos do
+    depends_on xcode: :build
+    uses_from_macos "swift" => :build
+
     url "https://github.com/wendylabsinc/wendy-agent/releases/download/2025.10.24-142919/wendy-cli-macos-arm64.tar.gz"
     sha256 "cbe1d957dccc003d84cce2e58040462379fb24cf34016ed07c107c99ad2e7bab"
 
@@ -14,6 +17,9 @@ cask "wendy" do
   end
 
   on_linux do
+    depends_on "swift"
+    depends_on "swiftly" => :build
+
     if Hardware::CPU.arm?
       url "https://github.com/wendylabsinc/wendy-agent/releases/download/2025.10.24-142919/wendy-cli-linux-static-musl-aarch64.tar.gz"
       sha256 "a52f35021d00acc8d56c42661f14aa27023cfef5f6daeeb3ccb97e1ee1e28f18"
