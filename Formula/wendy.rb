c16b94c0086f423570cd2e720f32eb2b8a3f9864
Joannis Orlandos <joannis@orlandos.nl>
2026-03-10 17:03:09 +0000

    wendy: update 2026.02.06-150016 bottle.

--- Formula/wendy.rb
+++ Formula/wendy.rb
@@ -3,9 +3,9 @@ class Wendy < Formula
   homepage "https://github.com/wendylabsinc/wendy-agent"
 
   bottle do
-    root_url "https://github.com/wendylabsinc/homebrew-tap/releases/download/wendy-2026.03.10-165737"
-    rebuild 2
-    sha256 cellar: :any_skip_relocation, arm64_tahoe: "1da03c57dd93ca8a57f8a04a87f21cc9b5ee64fb3ebd829618d328b3618934a1"
+    root_url "https://github.com/wendylabsinc/homebrew-tap/releases/download/wendy-2026.02.06-150016"
+    rebuild 3
+    sha256 cellar: :any_skip_relocation, arm64_tahoe: "5a20a8f19c058c28ed25bd7b9a178e83480bb055fc32e3fa56553aec6d6dd31c"
   end
 
   # Use pre-built binaries for all platforms
